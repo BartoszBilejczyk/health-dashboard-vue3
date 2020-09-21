7fd8d2a63b20b2f38a4c418fba2e2006c86c1b18
Bartosz Bilejczyk <bartoszbilejczyk@gmail.com>
2020-09-21 14:09:59 +0200

    Suspense used in Dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <template>
   <div id="app" class="app">
-    <Suspense>
-      <div class="app__content">
-        <AppNav class="app__nav"></AppNav>
-        <main class="app__main">
-          <router-view class="app__router-view"/>
-        </main>
-      </div>
-      <template #fallback>
-        <div class="app__loading">Loading...</div>
-      </template>
-    </Suspense>
+    <AppNav class="app__nav"></AppNav>
+    <main class="app__main">
+      <router-view class="app__router-view"/>
+    </main>
   </div>
 </template>
 
@@ -33,10 +26,6 @@
     display: flex;
     background: $color-primary-background--light;
 
-    &__content {
-      display: flex;
-    }
-
     &__nav {
       width: 100px;
       box-shadow: 0 8px 22px 0 rgba(132, 137, 155, 0.09);
@@ -46,25 +35,10 @@
       width: calc(100vw - 100px);
     }
 
-    &__header {
-      height: 90px;
-      width: 100%;
-    }
-
     &__router-view {
       height: 100vh;
       width: 100%;
       background: $color-primary-background;
-    }
-
-    &__loading {
-      height: 100vh;
-      width: 100%;
-      background: $color-primary-background;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      @include font(20, 500);
     }
   }
 </style>
